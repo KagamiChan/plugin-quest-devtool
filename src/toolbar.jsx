1e40d7195b6c192428dc4377c1a009cea4bbd5f9
lawvs <18554747+lawvs@users.noreply.github.com>
2018-03-20 16:39:22 +0800

    add fetch data

--- src/toolbar.jsx
+++ src/toolbar.jsx
@@ -15,8 +15,25 @@ export default class Toolbar extends Component {
     dataStore: PropTypes.observableObject.isRequired,
   }
 
+  state = {
+    fetchLoading: false,
+  }
+
   handleClean = () => {
     this.props.dataStore.update('');
+  }
+
+  handleFetchData = async () => {
+    this.setState({ fetchLoading: true });
+    try {
+      const resp = await fetch('https://kcwikizh.github.io/kcdata/quest/poi.json');
+      const content = await resp.text();
+      this.props.dataStore.update(content);
+    } catch (e) {
+      console.warn(e, e.stack); // eslint-disable-line no-console
+    } finally {
+      this.setState({ fetchLoading: false });
+    }
   }
 
   render() {
@@ -28,6 +45,14 @@ export default class Toolbar extends Component {
           onClick={this.handleClean}
         >
           清空数据
+        </Button>
+        <Button
+          icon="download"
+          disabled={!!this.props.dataStore.data}
+          onClick={this.handleFetchData}
+          loading={this.state.fetchLoading}
+        >
+          加载线上数据
         </Button>
       </ToolbarWrapper>
     );
